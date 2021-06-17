a1f9ea0e1fd9e408aed0cfcab6452e4f55e5ea36
Cherish Truong <truong@berkeley.edu>
2021-06-17 09:04:12 -0700

    Added Identity scaffolding

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Availibility.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Availibility.Areas.Identity.IdentityHostingStartup))]
+namespace Availibility.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AvailibilityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AvailibilityContextConnection")));
+
+                //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                //    .AddEntityFrameworkStores<AvailibilityContext>();
+            });
+        }
+    }
+}
